8b9b18b5beda98c92f440e6b21eb615be22a8564
MaikonRodriguesDeLima <maikonrdl@gmail.com>
2022-05-19 20:08:34 -0300

    site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";  
+import Footer from "./components/Footer";
+import Home from "./pages";
+import Menu from "./pages/menu";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import DropDown from "./components/DropDown";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth < 768 && isOpen) {
+        setIsOpen(false);
+      }
+      };
+      window.addEventListener('resize', hideMenu);
+      return () => {
+        window.removeEventListener('resize', hideMenu);
+      };
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar toggle={toggle} />
+        <DropDown isOpen={isOpen} toggle={toggle}/>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "./public/manifest.json", "./public/service-worker.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
